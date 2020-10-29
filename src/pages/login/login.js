d64ea4474f6642c4e483f1457a8da5cb339f77e4
owlyme <1148452777@qq.com>
2020-10-29 12:08:28 +0800

    login page logic

---
+++ src/pages/login/login.js
@@ -0,0 +1,96 @@
+// 全局app实例
+import { getNodeVale } from '../../utils/commom';
+import { Toast, Router } from '../../utils/sysApis';
+import Base from '../../utils/base';
+
+const getPhoneNumber = getNodeVale('phone');
+const getCode = getNodeVale('code');
+
+
+
+Page({
+  data: {
+    focus: false,
+    disabled: false,
+    phone: '',
+    code: null,
+    checked: true
+  },
+  onLoad() {
+    // Do some initialize when page load.
+  },
+  onReady() {
+    // Do something when page ready.
+  },
+  onShow() {
+    // Do something when page show.
+  },
+  onHide() {
+    // Do something when page hide.
+  },
+  onUnload() {
+    // Do something when page close.
+  },
+  onShareAppMessage() {
+    // return custom share data when user share.
+  },
+  onPhoneChange: function (e) {
+    console.log(this.data, getPhoneNumber(e));
+    this.setData({
+      ...getPhoneNumber(e)
+    });
+  },
+  onCodeChange: function (e) {
+    console.log(this.data, getCode(e));
+    this.setData({
+      ...getCode(e)
+    });
+  },
+  toAgree() {
+    this.setData({
+      checked: !this.data.checked
+    });
+  },
+  login() {
+    let {phone, code, checked} = this.data;
+    const validatoForm = () => {
+      let bool = true;
+      if(!Base.isPhone(phone)){
+        bool = false;
+        Toast.show({
+          title: '输入正确的手机号'
+        });
+      } else if (!code) {
+        bool = false;
+        Toast.show({
+          title: '输入验证码'
+        });
+      } else if (!checked) {
+        bool = false;
+        Toast.show({
+          title: '请同意服务协议与隐私政策'
+        });
+      }
+      return bool;
+    };
+    if (validatoForm()) {
+      let formData = {phone, code};
+      console.log(formData);
+      this.setData({
+        disabled: true
+      });
+      Router.refresh('/pages/index/index');
+
+      this.setData({
+        disabled: false
+      });
+    }
+  },
+  toServiceAgreementPage() {
+    Router.push('/pages/serviceAgreement/serviceAgreement');
+  },
+  toPrivacyPolicyPage() {
+    Router.push('/pages/privacyPolicy/privacyPolicy');
+  },
+  customData: {}
+});
---
+++ src/utils/commom.js
@@ -0,0 +1,8 @@
+// 获取data值
+export const getNodeData = dataName => event => event.currentTarget.dataset[dataName];
+// 获取input Value值
+export const getInputValue = event => event.detail.value;
+// 获取 input 的信息 返回 {dataName: value}
+export const getNodeVale = (dataName) => event => ({
+  [event.currentTarget.dataset[dataName]]: getInputValue(event)
+});
