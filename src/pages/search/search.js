999d47f090ef60c5a3231284a6ac282e5bec0daf
owlyme <1148452777@qq.com>
2020-11-10 11:17:43 +0800

    find search page

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,10 +1,34 @@
 // 全局app实例
+import { storageKeyMap, getLocalStorage, setLocalStorage, addKeywordsHistory } from '../../utils/localStorage';
 import PagePathes from '../../router/index'
-const app = getApp();
-console.log(app);
+import { Router } from '../../utils/sysApis'
+import { getDatasetValue } from '../../utils/commom'
+const App = getApp();
+const listType = getDatasetValue('type')
+
+
+function getHistoryKeywordsList() {
+  return getLocalStorage(storageKeyMap.keywords) || []
+}
 
 Page({
-  data: {},
+  data: {
+    offsetTop: App.globalData.navHeight,
+    historyList: getHistoryKeywordsList(),
+    findedList: [
+      1,
+      2,3,4,5,6,7,
+      '华为手机',
+      '苹果手机',
+      '吃不完的零食大礼包',
+      '苹果手机',
+      '吃不完的零食大礼包',
+      '苹果手机',
+      '吃不完的零食超大超大超大超大超大超大超大超大超大超',
+    ],
+    remindList: [  1,2,3,4,5,6,7,8,9,10,11,12,13,1,4,1,1,1,1,1,11,],
+    keyword: ''
+  },
   onLoad() {
     // Do some initialize when page load.
   },
@@ -35,5 +59,58 @@ Page({
   onTabItemTap() {
     // 当前是 tab 页时，点击 tab 时触发
   },
-  customData: {}
+  customData: {},
+  
+  // 输入框变化时
+  onSearchInputChange(e) {
+    let keyword = e.detail.keyword
+    this.setData({keyword})
+    //2 获取remindList
+    
+  },
+  remind(e) {
+    let keyword = e.currentTarget.dataset.item
+    console.log(e, keyword)
+    addKeywordsHistory(keyword)
+
+    setTimeout(() => {
+      this.setData({
+        historyList: getHistoryKeywordsList()
+      })
+    }, 100)
+
+    Router.push(PagePathes.categoryGoodList)
+  },
+  deleteHistory() {
+    setLocalStorage(storageKeyMap.keywords, [])
+    this.setData({
+      historyList: []
+    })
+
+  },
+  onSearch(e) {
+    console.log(e)
+    setTimeout(() => {
+      this.setData({
+        historyList: getHistoryKeywordsList()
+      })
+    }, 100)
+    Router.push(PagePathes.categoryGoodList)
+  },
+  onKeywordChange(e) {
+    console.log(e)
+
+    let type = listType(e)
+    if (type === 'finded') {
+      addKeywordsHistory(e.detail.keyword)
+    }
+
+    setTimeout(() => {
+      this.setData({
+        historyList: getHistoryKeywordsList()
+      })
+    }, 100)
+
+    Router.push(PagePathes.categoryGoodList)
+  }
 });
